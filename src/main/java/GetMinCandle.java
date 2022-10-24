fd5e5e2c3f35d040f04da44d9e0970d27619cc0c
SJaeYub <elapid0212@naver.com>
2022-10-24 20:20:28 +0900

    20221024 20:19 분봉

---
+++ src/main/java/GetMinCandle.java
@@ -0,0 +1,54 @@
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.security.PrivateKey;
+import java.util.UUID;
+
+import personnelFolder.PersonnelKey;
+
+
+public class GetMinCandle {
+
+    public static void main(String[] args) {
+        PersonnelKey pk = new PersonnelKey();
+
+        String accessKey = pk.getAccKey();
+        String secretKey = pk.getSecKey();
+        String serverUrl = "https://api.upbit.com";
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        String jwtToken = JWT.create()
+                .withClaim("access_key", accessKey)
+                .withClaim("nonce", UUID.randomUUID().toString())
+                .sign(algorithm);
+
+        String authenticationToken = "Bearer " + jwtToken;
+
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(serverUrl + "/v1/candles/minutes/1?market=KRW-BTC&count=1");
+            request.setHeader("Content-Type", "application/json");
+            request.addHeader("Authorization", authenticationToken);
+
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            System.out.println(EntityUtils.toString(entity, "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
