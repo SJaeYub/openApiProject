69056dbd6bfe402b8cc358c8539f3ddfcca27df9
SJaeYub <elapid0212@naver.com>
2023-04-03 21:38:51 +0900

    20230403 매수 예비 시그널 구현 1

--- src/main/java/tradingStrategy/TradingStrService.java
+++ src/main/java/tradingStrategy/TradingStrService.java
@@ -1,10 +1,12 @@
 package tradingStrategy;
 
 import callAPI.GetMinCandle;
+import cmn.CSVReader;
 import com.google.gson.JsonElement;
 import parsing.JsonArrayParser;
 import personnelFolder.PersonnelKey;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class TradingStrService {
@@ -17,12 +19,96 @@ public class TradingStrService {
 
     private String accKey = new PersonnelKey().getAccKey();
     private String secKey = new PersonnelKey().getSecKey();
+
+    public static void main(String[] args) {
+
+        TradingStrService strService = new TradingStrService();
+        strService.tempReverseTrading();
+    }
+
     public void tempReverseTrading() {
         GetMinCandle minCandle = new GetMinCandle(accKey, secKey);
-        String minCandle1 = minCandle.getMinCandle("BTC-XRP", null, "1", "30");
-        System.out.println(minCandle1);
 
+//            String minCandle1 = minCandle.getMinCandle("BTC-XRP", null, "1", "30");
+//            System.out.println(minCandle1);
 
+        CSVReader csvReader = new CSVReader();
+        List<List<String>> lists = csvReader.readCSV("/Users/user/IdeaProjects/openApiProject/src/main/java/personnelFolder/FINAL_USO.csv");
+
+        //항상 전일 양-음봉을 가지고 판단
+        boolean buy_flag = false;
+        boolean sell_flag = false;
+        boolean buy_position = false;
+        boolean sell_position = false;
+
+        int cnt = 0;
+        BigDecimal init_enter_price = new BigDecimal(0);
+
+        for (int i = 3; i < lists.size(); i++) {
+
+            if(cnt != 0 && cnt < 6) {
+                cnt++;
+            } else {
+                cnt = 0;
+                buy_flag = false;
+            }
+
+            BigDecimal today_open = new BigDecimal(lists.get(i).get(1));
+            BigDecimal today_high = new BigDecimal(lists.get(i).get(2));
+            BigDecimal today_low = new BigDecimal(lists.get(i).get(3));
+            BigDecimal today_close = new BigDecimal(lists.get(i).get(4));
+
+//            System.out.println(lists.get(i).get(0) + " " + open + " " +
+//                    high + " " + low + " " + close);
+
+            BigDecimal yesterday_open = new BigDecimal(lists.get(i - 1).get(1));
+            BigDecimal yesterday_high = new BigDecimal(lists.get(i - 1).get(2));
+            BigDecimal yesterday_low = new BigDecimal(lists.get(i - 1).get(3));
+            BigDecimal yesterday_close = new BigDecimal(lists.get(i - 1).get(4));
+
+            BigDecimal twoDaysAgo_open = new BigDecimal(lists.get(i - 2).get(1));
+            BigDecimal twoDaysAgo_high = new BigDecimal(lists.get(i - 2).get(2));
+            BigDecimal twoDaysAgo_low = new BigDecimal(lists.get(i - 2).get(3));
+            BigDecimal twoDaysAgo_close = new BigDecimal(lists.get(i - 2).get(4));
+
+            boolean twoDaysAgo_signal = determine_candle(twoDaysAgo_open, twoDaysAgo_close);
+            boolean pre_signal = determine_candle(yesterday_open, yesterday_close);
+            boolean curr_signal = determine_candle(today_open, today_close);
+
+            //두 봉이 연속 양봉이면서 당일 봉이 음봉인 경우
+            if (pre_signal && !curr_signal && twoDaysAgo_signal) {
+                buy_flag = true;
+
+                init_enter_price = yesterday_high;
+
+                cnt = 1;
+            }
+
+            if (buy_flag) {
+                if (init_enter_price.compareTo(today_high) == 1) {
+                    //buying
+
+                }
+            }
+
+        }
+    }
+
+    /**
+     * 양-음봉 결정
+     *
+     * @param open
+     * @param close 시가보다 종가가 높거나 같으면 양봉, 낮으면 음봉 판단
+     * @return
+     */
+    public boolean determine_candle(BigDecimal open, BigDecimal close) {
+        boolean result = false;
+
+        if (close.compareTo(open) >= 0) {
+            result = true;
+        }
+
+        return result;
     }
 
     public String getMarketCode(String target) {
